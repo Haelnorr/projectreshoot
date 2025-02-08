68754991c81e96ce80ca07f0e01c3afa78eb2ba8
Haelnorr <lockedonohoe@gmail.com>
2025-02-09 00:50:58 +1100

    Added reminder to tester to avoid tracking changes

--- tester.go
+++ tester.go
@@ -10,6 +10,8 @@ import (
 // This function will only be called if the --test commandline flag is set.
 // After the function finishes the application will close.
 // Running command `make test` will run the test using port 3232 to avoid
-// conflicts on the default 3333. Useful for testing things out during dev
+// conflicts on the default 3333. Useful for testing things out during dev.
+// If you add code here, remember to run:
+// `git update-index --assume-unchanged tester.go` to avoid tracking changes
 func test(config *server.Config, conn *sql.DB, srv *http.Server) {
 }

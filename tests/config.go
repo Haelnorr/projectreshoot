e3d2eb1af86bcc23163841ed78d8b2947fc6c6da
Haelnorr <lockedonohoe@gmail.com>
2025-03-01 21:33:43 +1100

    Fixed tmdb token not set causes tests to fail

--- tests/config.go
+++ tests/config.go
@@ -9,6 +9,7 @@ import (
 
 func TestConfig() (*config.Config, error) {
 	os.Setenv("SECRET_KEY", ".")
+	os.Setenv("TMDB_API_TOKEN", ".")
 	cfg, err := config.GetConfig(map[string]string{})
 	if err != nil {
 		return nil, errors.Wrap(err, "config.GetConfig")
